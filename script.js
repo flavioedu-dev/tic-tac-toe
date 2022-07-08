d4c5ef3b6a264e310af42e00d329a5663b6cdfd5
Flávio Eduardo <xflax00@gmail.com>
2022-07-08 20:51:17 -0300

    Iniciando projeto

---
+++ script.js
@@ -0,0 +1,86 @@
+click = document.getElementById("fContainer")
+
+const seq = [["q1","q2","q3"],["q4","q5","q6"],["q7","q8","q9"],["q1","q4","q7"],["q2","q5","q8"],["q3","q6","q9"],["q1","q5","q9"],["q3","q5","q7"]]
+
+let cont = 0
+
+click.addEventListener('click', (e) => {
+    
+    for(let i=0; i < click.children.length; i++){
+        /* console.log(click.children[i]) */
+
+        for(let j=0; j < click.children[i].children.length; j++){
+            /* console.log(click.children[i].children[j]) */
+
+            if(e.target == click.children[i].children[j]){
+
+                if(cont%2==0){
+                    let iX = new Image()
+                    iX.src = "img/X.webp"
+                    iX.classList.add("p1")
+                    iX.style.maxWidth = "100px"
+                    e.target.appendChild(iX)
+
+                }else{
+                    let iBola = new Image()
+                    iBola.src = "img/Bola.webp"
+                    iBola.classList.add("p2")
+                    iBola.style.maxWidth = "100px"
+                    e.target.appendChild(iBola)
+
+                }
+
+                cont++
+
+            }
+
+        }
+    }
+
+    for (k in seq){
+
+        /* console.log(seq[k]) */
+
+        let jog1 = 0
+        let jog2 = 0
+
+        for(l in seq[k]){
+            console.log(document.getElementById(seq[k][l]))
+            if(document.getElementById(seq[k][l]).childElementCount == 1){
+                console.log("ok")
+                if(document.getElementById(seq[k][l]).children[0].className == 'p1'){
+                    jog1++
+                }else if(document.getElementById(seq[k][l]).children[0].className == 'p2'){
+                    jog2++
+                }
+                console.log(jog1)
+
+                if(jog1 == 3){
+                    click.style.pointerEvents = "none"
+                    setTimeout(() => {
+                        alert("Jogador 1 venceu!")
+                        confirm("Quer iniciar um novo jogo?")?document.location.reload(true): console.log("ok")
+                    }, 100)
+                    
+                }else if(jog2 == 3){
+                    click.style.pointerEvents = "none"
+                    setTimeout(() => {
+                        alert("Jogador 2 venceu!")
+                        confirm("Quer iniciar um novo jogo?")?document.location.reload(true): console.log("ok")
+                    }, 100)
+
+                }
+
+            }
+            
+        }
+        
+    }
+    if(cont == 9){
+        setTimeout(() => {
+            alert("Deu zebra!")
+            confirm("Quer iniciar um novo jogo?")?document.location.reload(true): console.log("ok")
+        }, 100)
+    }
+
+})
