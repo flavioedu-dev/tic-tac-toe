205ca1ffa3a6e439200f0b19601a4d403e1ad7b4
Flávio Eduardo <xflax00@gmail.com>
2022-07-11 13:25:19 -0300

    +Score

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 click = document.getElementById("fContainer")
 let statusJg = document.getElementById("status")
+let btnRnc = document.getElementById("btnRnc")
+let history = document.getElementById("history")
 
 click.style.pointerEvents = "none"
 
+function score(){
+    let jogNames = document.querySelectorAll(".jogName")
+
+    jogNames[0].innerHTML = jogName1
+    jogNames[1].innerHTML = jogName2
+}
+
 function clickBtn(){
+
+    setTimeout(() => {
+        click.style.visibility = "visible"
+        statusJg.style.visibility = "visible"
+        btnRnc.style.visibility = "visible"
+        history.style.visibility = "visible"
+    }, 100)
+
+    document.getElementById("equipe").style.visibility = "hidden"
+
     for(let i=0; i < click.children.length; i++){
 
         for(let j=0; j < click.children[i].children.length; j++){
 
-            console.log(click.children[i].children[j])
+            /* console.log(click.children[i].children[j]) */
             
             if(click.children[i].children[j].childElementCount != 0){
                 click.children[i].children[j].children[0].remove()      
@@ -19,8 +38,11 @@ function clickBtn(){
         }
     
     }
+    score()
+
     cont = 0
 }
+
 function getName(){
     jogName1 = document.getElementById("jg1").value
     jogName2 = document.getElementById("jg2").value
@@ -49,7 +71,7 @@ function newJogo(){
     let nJogo = confirm("Quer iniciar um novo jogo?")
 
     if(nJogo == true){
-        console.log("Sim")
+        /* console.log("Sim") */
 
         click.style.pointerEvents = "all"
 
@@ -57,7 +79,7 @@ function newJogo(){
 
             for(let j=0; j < click.children[i].children.length; j++){
 
-                console.log(click.children[i].children[j])
+                /* console.log(click.children[i].children[j]) */
                 
                 if(click.children[i].children[j].childElementCount != 0){
                     click.children[i].children[j].children[0].remove()
@@ -69,14 +91,25 @@ function newJogo(){
             }
         
         }
-    } 
+        let jogPonts = document.querySelectorAll(".jogPont")
+        jogPonts[0].innerHTML = Win1
+        jogPonts[1].innerHTML = Win2
+
+    }
     
+}
+
+function Rnc(){
+    location.reload()
 }
 
 const seq = [["q1","q2","q3"],["q4","q5","q6"],["q7","q8","q9"],["q1","q4","q7"],["q2","q5","q8"],["q3","q6","q9"],["q1","q5","q9"],["q3","q5","q7"]]
 
 let cont = 0
 let win = 0
+let Win1 = 0
+let Win2 = 0
+let zebra = 0
 
 click.addEventListener('click', (e) => {
     
@@ -123,6 +156,7 @@ click.addEventListener('click', (e) => {
 
         let jog1 = 0
         let jog2 = 0
+        win = 0
 
         for(l in seq[k]){
 
@@ -137,6 +171,7 @@ click.addEventListener('click', (e) => {
                 if(jog1 == 3){
                     click.style.pointerEvents = "none"
                     win = 1
+                    Win1++
                     
                     for(x in seq[k]){
                         document.getElementById(seq[k][x]).style.backgroundColor = "rgb(204, 18, 18)"
@@ -150,6 +185,7 @@ click.addEventListener('click', (e) => {
                 }else if(jog2 == 3){
                     click.style.pointerEvents = "none"
                     win = 1
+                    Win2++
 
                     for(x in seq[k]){
                         document.getElementById(seq[k][x]).style.backgroundColor = "rgb(204, 18, 18)"
@@ -167,11 +203,25 @@ click.addEventListener('click', (e) => {
         }
         
     }
-    if(cont == 9 & win ==0){
-        setTimeout(() => {
-            alert("Deu zebra!")
-            confirm("Quer iniciar um novo jogo?")?document.location.reload(true): console.log("ok")
-        }, 100)
+
+    zebra = 0
+
+    for(let i=0; i < click.children.length; i++){
+
+        for(let j=0; j < click.children[i].children.length; j++){
+            
+            if(click.children[i].children[j].childElementCount != 0){
+                zebra++
+            }
+            
+            if(zebra==9 & win<=Win1+Win2){
+                setTimeout(() => {
+                    alert("Deu zebra!")
+                    newJogo()
+                }, 100)
+            }
+        }
+    
     }
 
 })
